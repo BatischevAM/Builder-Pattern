ccb9f7644bc3dd4de62a192df3ac7711d25e020e
BatischevAM <batischevam@mer.ci.nsu.ru>
2024-10-14 13:55:49 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Builder_pattern;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Processor cpu = new Processor { Model = "13th Gen Intel(R) Core(TM) i5 13600KF", Cores = 14 };
+        Motherboard motherboard = new Motherboard { Model = "GIGABYTE B760M DS#H AX DDR4" };
+        RAM ram = new RAM { Size = "32GB" };
+        Storage storage1 = new Storage { Type = "HDD", Size = "4TB" };
+        Storage2 storage2 = new Storage2 { Type = "SSD", Size = "1TB" };
+        Videocard videoCard = new Videocard { Model = "NVIDIA GeForce RTX 4060 Ti" };
+
+        ComputerBuilder builder = new ComputerBuilder();
+        Computer myComputer = builder.SetCPU(cpu)
+            .SetMotherboard(motherboard)
+            .SetRAM(ram)
+            .SetStorage(storage1)
+            .SetStorage2(storage2)
+            .SetVideoCard(videoCard)
+            .Build();
+        Console.WriteLine(myComputer);
+    }
+}
